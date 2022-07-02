9393c3b3ebbdf0d556818f1f71659762c7810977
Am-Awal <am.awal@outlook.com>
2022-07-01 19:37:13 -0500

    Initial Commit

---
+++ GoalDefinite/GoalDefiniteApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalDefiniteApp.swift
+//  GoalDefinite
+//
+//  Created by Awal Amadou on 7/1/22.
+//
+
+import SwiftUI
+
+@main
+struct GoalDefiniteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
